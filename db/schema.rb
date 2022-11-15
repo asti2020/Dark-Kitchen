a826d65985e101404499aa7484a24f7385dbeab5
Ameen.Maria.S@gmail.com <redmomo@Marias-MacBook-Air.local>
2022-11-15 10:12:38 -0500

    Fixed Seed typos so tables can be seeded; seeded tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_11_203754) do
+ActiveRecord::Schema.define(version: 2022_11_15_145556) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "product_id"
+    t.boolean "complete"
+    t.float "total_price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.float "price"
+    t.integer "quantity"
+    t.string "ingredients"
+    t.string "allergens"
+    t.text "description"
+    t.string "category"
+    t.integer "calories"
+    t.string "image_url"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "full_name"
