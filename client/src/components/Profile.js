155bc60232e077f3f34f12aee6651e43020a4b12
George Fernandez <fernandezg704@gmail.com>
2022-11-16 10:03:53 -0500

    Profile page styling

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -21,9 +21,7 @@ function Profile({user}) {
 
 
             <div id='leftColumn'>
-                {/* <img className='leftImages' src='https://loveincorporated.blob.core.windows.net/contentimages/gallery/d9e900e4-212e-4c3d-96d5-cb14a023c659-worlds-most-delicious-dishes.jpg' alt='food dish'></img>
-                <h3>Title</h3>
-                <p>Description</p> */}
+
             </div>
 
             <div id='middleColumn'>
