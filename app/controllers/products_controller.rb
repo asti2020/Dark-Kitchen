0aeada9a293544587bef4cdf04d5de4dbb09af40
Ameen.Maria.S@gmail.com <redmomo@Marias-MacBook-Air.local>
2022-11-16 11:16:30 -0500

    products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -43,7 +43,7 @@ class ProductsController < ApplicationController
      def destroy
         @product.destroy
         respond_to do |format|
-          format.html { redirect_to products_url, notice: 'pProduct was successfully destroyed.' }
+          format.html { redirect_to products_url, notice: 'Product was successfully destroyed.' }
           format.json { head :no_content }
         end
     end
