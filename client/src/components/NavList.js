3987837f8c21ddf02e72d602c06b2f6a556f9e34
George Fernandez <fernandezg704@gmail.com>
2022-11-17 10:02:25 -0500

    morecss

--- client/src/components/NavList.js
+++ client/src/components/NavList.js
@@ -7,11 +7,6 @@ import { NavLink } from 'react-router-dom'
 function NavList() {
   // This styling will be applied to a <NavLink> when the
   // route that it links to is currently selected.
-  let activeStyle = {
-    textDecoration: "none",
-  };
-
-  let activeClassName = "nav-link-active";
 
   return (
     <nav id='navBar'>
